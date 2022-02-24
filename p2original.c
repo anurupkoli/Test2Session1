2325adab459bc6da972a44131c453ecca09ce2eb
anurupkoli <97871350+anurupkoli@users.noreply.github.com>
2022-02-24 08:47:45 +0000

    jh

--- p2original.c
+++ p2original.c
@@ -7,7 +7,7 @@ void input_line(float *x1, float *y1)
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-  int a,b,c;
+  float a,b,c;
   a=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
   b=sqrt(pow((x3-x2),2)+pow((y3-y2),2));
   c=sqrt(pow((x1-x3),2)+pow((y1-y3),2));
@@ -29,7 +29,7 @@ int main()
   input_line(&x1,&y1);
   input_line(&x2,&y2);
   input_line(&x3,&y3);
-  int a=is_triangle(x1,y1,x2,y2,x3,y3);
+  float a=is_triangle(x1,y1,x2,y2,x3,y3);
   output(x1,y1,x2,y2,x3,y3,a);
   return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int input_number(){
+  int n;
+  printf("please enter a number\n");
+  scanf("%d", &n);
+
+  return n;
+}
+
+int is_prime(int n){
+  int i, o = 0;
+
+  for(i=0; i<=n; i++){
+    
+  }
+}
